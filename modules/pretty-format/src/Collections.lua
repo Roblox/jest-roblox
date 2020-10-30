45125409e6f4ff0bb73243de2940172b1411b137
Anonymous Author <roflection-bot@roblox.com>
2020-10-30 16:10:08 -0700

    ADO-1138 add pretty-format 26.5.2 (#7)
    Adds the pretty-format package. This PR doesn't include support for pretty-format plugins. This PR includes the external type definitions in `Types.lua` but doesn't currently use them. These will be addressed in a later PR. 
    Our pretty-format supports the Roblox `DateTime` type. CI workflow is changed to pass in the `UseDateTimeType3`.
    Formatting for most JS types are not supported. Color formatting is also not supported.
    
    Some major deviations to be aware of: we don't distinguish between any of the Lua table-like objects and format all of them as `Table`. We also prefer to use the Lua native string representation for primitives like `nan`, `inf` and `nil`, so the tests are modified accordingly.
    
    Check the README for pretty-format for more detail on deviations from upstream.

---
+++ modules/pretty-format/src/Collections.lua
@@ -0,0 +1,123 @@
+--!nonstrict
+-- upstream: https://github.com/facebook/jest/blob/v26.5.3/packages/pretty-format/src/collections.ts
+-- /**
+--  * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+--  *
+--  * This source code is licensed under the MIT license found in the
+--  * LICENSE file in the root directory of this source tree.
+--  *
+--  */
+
+local Collections = {}
+
+-- local Types = require(script.Types)
+
+-- deviation: deviates from upstream substantially since Lua only has tables
+-- we only have two functions
+-- `printTableEntries` for formatting key, value pairs and
+-- `printListItems` for formatting arrays
+
+-- deviation: printIteratorEntries is renamed to printTableEntries
+-- /**
+--  * Return entries (for example, of a map)
+--  * with spacing, indentation, and comma
+--  * without surrounding punctuation (for example, braces)
+--  */
+function Collections.printTableEntries(
+	t: any,
+	config,
+	indentation: string,
+	depth: number,
+	refs,
+	printer,
+	-- // Too bad, so sad that separator for ECMAScript Map has been ' => '
+	-- // What a distracting diff if you change a data structure to/from
+	-- // ECMAScript Object or Immutable.Map/OrderedMap which use the default.
+	separator: string
+): string
+	local result = ''
+
+	-- deviation: rewritten with a for ... pairs instead of an iterator
+	local keys = {}
+	for k, _ in pairs(t) do
+		table.insert(keys, k)
+	end
+
+	if #keys > 0 then
+		result = result .. config.spacingOuter
+
+		local indentationNext = indentation .. config.indent
+
+		for i = 1, #keys do
+			local k = keys[i]
+			local v = t[k]
+			local name = printer(
+				k,
+				config,
+				indentationNext,
+				depth,
+				refs
+			)
+			local value = printer(
+				v,
+				config,
+				indentationNext,
+				depth,
+				refs
+			)
+
+			result = result .. indentationNext .. name .. separator .. value
+
+			if i < #keys then
+				result = result .. ',' .. config.spacingInner
+			elseif not config.min then
+				result = result .. ','
+			end
+		end
+
+		result = result .. config.spacingOuter .. indentation
+	end
+
+	return result
+end
+
+-- /**
+--  * Return items (for example, of an array)
+--  * with spacing, indentation, and comma
+--  * without surrounding punctuation (for example, brackets)
+--  **/
+function Collections.printListItems(
+	list: { [number]: any },
+	config,
+	indentation: string,
+	depth: number,
+	refs,
+	printer
+): string
+	local result = ''
+
+	if #list > 0 then
+		result = result .. config.spacingOuter
+
+		local indentationNext = indentation .. config.indent
+
+		for i = 1, #list do
+			result = result ..
+				indentationNext ..
+				printer(list[i], config, indentationNext, depth, refs)
+
+			-- deviation: < #list instead of #list - 1 because of 1-indexing
+			if i < #list then
+				result = result .. ',' .. config.spacingInner
+			elseif not config.min then
+				result = result .. ','
+			end
+		end
+
+		result = result .. config.spacingOuter .. indentation
+	end
+
+	return result
+end
+
+return Collections
