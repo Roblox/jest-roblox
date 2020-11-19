82271492abef9d9a4a27217ea629d9cd6292966f
Anonymous Author <roflection-bot@roblox.com>
2020-11-19 15:17:09 -0800

    NOJIRA exit early if static analyzer fails (#15)

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 rojo build jest.project.json --output model.rbxmx
 roblox-cli analyze jest.project.json
 roblox-cli run --load.model model.rbxmx --run bin/spec.lua --fastFlags.overrides "UseDateTimeType3=true"
