56c078d80ef1c0c90adba4ac35b039c720c405a6
Anonymous Author <roflection-bot@roblox.com>
2021-03-02 15:14:41 -0800

    Version 0.7.0 (#37)
    Changes to CI to remove .robloxrc from dev dependencies

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
 rojo build jest.project.json --output model.rbxmx
+find Packages/Dev -name "*.robloxrc" | xargs rm -f
+find Packages/_Index -name "*.robloxrc" | xargs rm -f
 roblox-cli analyze jest.project.json
 roblox-cli run --load.model model.rbxmx --run bin/spec.lua --fastFlags.overrides "UseDateTimeType3=true" --testService.errorExitCode=1
