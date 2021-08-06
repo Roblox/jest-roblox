047f45c747ce8a0dbb52928e5defa023bcbfda3b
Anonymous Author <roflection-bot@roblox.com>
2021-08-06 15:11:17 -0700

    clean up jest object

--- src/init.lua
+++ src/init.lua
@@ -1,42 +1,19 @@
-local Expectation = require(script.TestEZ.Expectation)
-local TestBootstrap = require(script.TestEZ.TestBootstrap)
-local TestEnum = require(script.TestEZ.TestEnum)
-local TestPlan = require(script.TestEZ.TestPlan)
-local TestPlanner = require(script.TestEZ.TestPlanner)
-local TestResults = require(script.TestEZ.TestResults)
-local TestRunner = require(script.TestEZ.TestRunner)
-local TestSession = require(script.TestEZ.TestSession)
-local TextReporter = require(script.TestEZ.Reporters.TextReporter)
-local TextReporterQuiet = require(script.TestEZ.Reporters.TextReporterQuiet)
-local TeamCityReporter = require(script.TestEZ.Reporters.TeamCityReporter)
+local TestEZ = require(script.TestEZ)
+local Globals = require(script.Globals)
 
-local function run(testRoot, callback)
-	local modules = TestBootstrap:getModules(testRoot)
-	local plan = TestPlanner.createPlan(modules)
-	local results = TestRunner.runPlan(plan)
+return {
+	run = TestEZ.run,
 
-	callback(results)
-end
+	Expectation = TestEZ.Expectation,
+	TestBootstrap = TestEZ.TestBootstrap,
+	TestEnum = TestEZ.TestEnum,
+	TestPlan = TestEZ.TestPlan,
+	TestPlanner = TestEZ.TestPlanner,
+	TestResults = TestEZ.TestResults,
+	TestRunner = TestEZ.TestRunner,
+	TestSession = TestEZ.TestSession,
 
-local TestEZ = {
-	run = run,
+	Reporters = TestEZ.Reporters,
 
-	Expectation = Expectation,
-	TestBootstrap = TestBootstrap,
-	TestEnum = TestEnum,
-	TestPlan = TestPlan,
-	TestPlanner = TestPlanner,
-	TestResults = TestResults,
-	TestRunner = TestRunner,
-	TestSession = TestSession,
-
-	Reporters = {
-		TextReporter = TextReporter,
-		TextReporterQuiet = TextReporterQuiet,
-		TeamCityReporter = TeamCityReporter,
-	},
+	Globals = Globals
 }
-
-TestEZ.Globals = require(script.Globals)
-
-return TestEZ
