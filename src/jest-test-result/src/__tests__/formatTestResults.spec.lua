1e26d0d45ea4ed4178b32396c699cfcda43bfb11
Anonymous Author <roflection-bot@roblox.com>
2022-02-28 12:43:40 -0500

    Feat/150 port jest-test-result (#155)
    * jest-test-result initial conversion
    
    * Added upstream comments
    
    * tests running but failing, work on fixing up types file
    
    * passing tests
    
    * fixes some type issues
    
    * Fixes last analyze type errors
    
    * Updates from review feedback
    
    * Adds .robloxrc file
    
    * Fixed new analyze issues raised after adding .robloxrc file
    
    * Adds tests for helpers
    
    * More review changes
    
    * Fixes deviation comment

---
+++ src/jest-test-result/src/__tests__/formatTestResults.spec.lua
@@ -0,0 +1,42 @@
+-- ROBLOX upstream: https://github.com/facebook/jest/blob/v27.4.7/packages/jest-test-result/src/__tests__/formatTestResults.test.ts
+--[[*
+ * Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ ]]
+return function()
+	local CurrentModule = script.Parent.Parent
+	local Packages = CurrentModule.Parent
+
+	local jestExpect = require(Packages.Dev.Expect)
+	local formatTestResults = require(CurrentModule.formatTestResults).default
+
+	local types = require(CurrentModule.types)
+	type AggregatedResult = types.AggregatedResult
+
+	describe("formatTestResults", function()
+		local assertion = {
+			fullName = "TestedModule#aMethod when some condition is met returns true",
+			status = "passed",
+			title = "returns true",
+		}
+
+		-- ROBLOX deviation START - casted to any, TS should fail here
+		local results: AggregatedResult = {
+			testResults = {
+				{
+					numFailingTests = 0,
+					perfStats = { end_ = 2, runtime = 1, slow = false, start = 1 }, -- @ts-expect-error
+					testResults = { assertion },
+				},
+			},
+		} :: any
+		-- ROBLOX deviation END
+
+		it("includes test full name", function()
+			local result = formatTestResults(results, nil, nil)
+			jestExpect(result.testResults[1].assertionResults[1].fullName).toEqual(assertion.fullName)
+		end)
+	end)
+end
