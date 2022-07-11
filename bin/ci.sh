1efe30b868de87d3720c1c63f9d9a50dbce15265
Anonymous Author <roflection-bot@roblox.com>
2022-07-11 13:23:40 -0700

    Remove dependency cycle (#206)
    This cycle caused rotriever to run forever when pulling jest into roact-alignment to test something >.>
    
    Obviously there's a rotriever fix to find and handle cycles more gracefully, but we'll also need to fix the cycle before we can start trying out the new alpha. I'm fine with overriding the existing alpha.0 tag since it'd be unusable anyways, but I can also bump to alpha.1 instead if desired.

--- bin/ci.sh
+++ bin/ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 selene src
 stylua -c src
-roblox-cli analyze jest.project.json --fastFlags.allOnLuau
-roblox-cli run --load.model jest.project.json --run bin/spec.lua --testService.errorExitCode=1 --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true
+robloxdev-cli analyze jest.project.json --fastFlags.allOnLuau
+robloxdev-cli run --load.model jest.project.json --run bin/spec.lua --testService.errorExitCode=1 --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true
 # roblox-cli run --load.model jest.project.json --run bin/spec.lua --testService.errorExitCode=1 --fastFlags.allOnLuau --lua.globals=UPDATESNAPSHOT="all" --load.asRobloxScript --fs.readwrite="$(pwd)" --fastFlags.overrides EnableLoadModule=true
