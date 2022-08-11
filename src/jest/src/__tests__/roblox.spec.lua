ec2f58fee3897da8394036c004c1be0a5d9ecef5
Anonymous Author <roflection-bot@roblox.com>
2022-08-11 18:42:01 +0200

    Feat/225 port jest main package (#226)
    * port jest main module
    * use jest main module to run tests

--- src/jest/src/__tests__/roblox.spec.lua
+++ src/jest/src/__tests__/roblox.spec.lua
@@ -1,24 +1,36 @@
+-- ROBLOX NOTE: no upstream
+
 local CurrentModule = script.Parent.Parent
 local Packages = CurrentModule.Parent
 
 local JestGlobals = require(Packages.Dev.JestGlobals)
-local jest = JestGlobals.jest
-local jestExpect = JestGlobals.expect
+local expect = JestGlobals.expect
 local describe = JestGlobals.describe
 local it = JestGlobals.it
 
-describe("Jest Object", function()
-	describe("methods are initialized", function()
-		describe("jestMock", function()
-			it("fn", function()
-				jestExpect(jest.fn).never.toBeNil()
-			end)
-			it("clearAllMocks", function()
-				jestExpect(jest.clearAllMocks).never.toBeNil()
-			end)
-			it("resetAllMocks", function()
-				jestExpect(jest.resetAllMocks).never.toBeNil()
-			end)
+local jestModule = require(CurrentModule)
+
+describe("Jest", function()
+	describe("re-exports JestCore", function()
+		it("SearchSource", function()
+			expect(jestModule.SearchSource).toBeDefined()
+		end)
+
+		it("TestWatcher", function()
+			expect(jestModule.TestWatcher).toBeDefined()
+		end)
+
+		it("createTestScheduler", function()
+			expect(jestModule.createTestScheduler).toBeDefined()
+		end)
+
+		-- ROBLOX NOTE: not ported
+		it.skip("getVersion", function()
+			-- expect(jestModule.getVersion).toBeDefined()
+		end)
+
+		it("runCLI", function()
+			expect(jestModule.runCLI).toBeDefined()
 		end)
 	end)
 end)
