7daa27c0d6cd5d4c4473aa51362513d00900d177
Anonymous Author <roflection-bot@roblox.com>
2022-09-15 08:31:15 -0700

    #nojiraFix PrettyFormatPluginError should throw on a plugin error (#250)

--- src/pretty-format/src/__tests__/roblox.spec.lua
+++ src/pretty-format/src/__tests__/roblox.spec.lua
@@ -20,6 +20,7 @@ local Packages = CurrentModule.Parent
 local JestGlobals = require(Packages.Dev.JestGlobals)
 local expect = JestGlobals.expect
 local it = JestGlobals.it
+local describe = JestGlobals.describe
 
 local prettyFormat = require(CurrentModule).default
 
@@ -39,6 +40,33 @@ it("Builtin Datatypes", function()
 	expect(prettyFormat(Color3.new(1, 1, 1))).toEqual("Color3(1, 1, 1)")
 	expect(prettyFormat(UDim2.new(1, 50, 0, 50))).toEqual("UDim2({1, 50}, {0, 50})")
 	expect(prettyFormat(Vector3.new(10, 20, 30))).toEqual("Vector3(10, 20, 30)")
+end)
+
+describe("bad plugin", function()
+	local badPlugin = {
+		test = function(val)
+			return val and val.foo ~= nil -- bad check, should be typeof(val) == "table"
+		end,
+		serialize = function(val, config, indentation, depth, refs, printer)
+			return "Pretty foo: " .. printer(val.foo, config, indentation, depth, refs, printer)
+		end,
+	}
+	local options: any = { plugins = { badPlugin } }
+
+	it("should throw PrettyFormatPluginError", function()
+		local bar = {
+			foo = {
+				a = 1,
+				b = 2,
+			},
+		}
+		local ok, result = pcall(function()
+			prettyFormat(bar, options)
+		end)
+		expect(ok).toBeFalsy()
+		expect(result.name).toBe("PrettyFormatPluginError")
+		expect(result.message).toMatch("attempt to index number with 'foo'")
+	end)
 end)
 
 return {}
