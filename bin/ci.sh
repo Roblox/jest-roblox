ddae9a41858ecd3ff29d6d64824fc2cc3a6460e0
Anonymous Author <roflection-bot@roblox.com>
2022-10-11 17:05:16 -0700

    LUAFDN-1312 - Deferred lua (#275)
    Problem
    Deferred Lua is coming next year and introduces subtle changes to how Roblox Lua code behaves in certain scenarios. https://jira.rbx.com/browse/LUAFDN-1312
    
    Solution
    We want tests that turn on deferred Lua ahead of time to proactively discover any issues

--- bin/ci.sh
+++ bin/ci.sh
@@ -8,5 +8,10 @@ stylua -c src
 robloxdev-cli analyze jest.project.json --fastFlags.allOnLuau
 
 echo "Run tests"
-robloxdev-cli run --load.model jest.project.json --run bin/spec.lua --testService.errorExitCode=1 --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true --lua.globals=UPDATESNAPSHOT=false --lua.globals=CI=false --load.asRobloxScript --fs.readwrite="$(pwd)"
+robloxdev-cli run --load.model jest.project.json \
+  --run bin/spec.lua --testService.errorExitCode=1 \
+  --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true \
+  EnableSignalBehavior=true DebugForceDeferredSignalBehavior=true MaxDeferReentrancyDepth=15 \
+  --lua.globals=UPDATESNAPSHOT=false --lua.globals=CI=false --load.asRobloxScript --fs.readwrite="$(pwd)"
+
 # robloxdev-cli run --load.model jest.project.json --run bin/spec.lua --testService.errorExitCode=1 --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true  --lua.globals=UPDATESNAPSHOT=true --lua.globals=CI=true --load.asRobloxScript --fs.readwrite="$(pwd)"
