b4f3b699b37440b96aac9b39d5228481b0f000e6
Anonymous Author <roflection-bot@roblox.com>
2022-11-08 12:57:58 +0100

    feat: jest.requireActual (#294)
    * feat: tests for requireActual (wip)
    
    * chore: stylua
    
    * fix: comment out unused variable
    
    * chore: renamed mocked-node-module
    
    * feat: enable requireActual with custom tests
    
    * chore: Update deviation comment with issue link

---
+++ src/jest-runtime/src/__tests__/requireActual.roblox.spec.lua
@@ -0,0 +1,60 @@
+local CurrentModule = script.Parent.Parent
+local Packages = CurrentModule.Parent
+local Promise = require(Packages.Promise)
+
+local JestGlobals = require(Packages.Dev.JestGlobals)
+local describe = JestGlobals.describe
+local expect = JestGlobals.expect
+local it = JestGlobals.it
+local beforeEach = JestGlobals.beforeEach
+local mockMeScript = script.Parent.mock_me
+
+local rootJsPath = script.Parent.test_root.root
+local __filename = mockMeScript
+local createRuntime
+
+describe("Roblox requireActual", function()
+	beforeEach(function()
+		createRuntime = require(CurrentModule.__mocks__.createRuntime)
+	end)
+
+	it("should mock module and then require the actual module", function()
+		return Promise.resolve():andThen(function()
+			local runtime = createRuntime(__filename):expect()
+			local root = runtime:requireModule(runtime.__mockRootPath, rootJsPath) -- Erase module registry because root.js requires most other modules.
+
+			root.jest.mock(mockMeScript, function()
+				return {
+					mocked = true,
+					actual = false,
+				}
+			end)
+
+			expect(runtime:requireModuleOrMock(mockMeScript).mocked).toEqual(true)
+			expect(runtime:requireModuleOrMock(mockMeScript).actual).toEqual(false)
+
+			local actualModule = root.jest.requireActual(mockMeScript)
+			expect(actualModule.mocked).toBe(false)
+			expect(actualModule.actual).toBe(true)
+		end)
+	end)
+
+	it("should mock module using part of the actual module", function()
+		return Promise.resolve():andThen(function()
+			local runtime = createRuntime(__filename):expect()
+			local root = runtime:requireModule(runtime.__mockRootPath, rootJsPath) -- Erase module registry because root.js requires most other modules.
+
+			-- this should resemble a typical usage of requireActual: i.e. you mock a module, but you also use parts of the actual module in the mock
+			root.jest.mock(mockMeScript, function()
+				local actual = root.jest.requireActual(mockMeScript)
+				return {
+					mocked = true,
+					actual = actual.actual,
+				}
+			end)
+
+			expect(runtime:requireModuleOrMock(mockMeScript).mocked).toEqual(true)
+			expect(runtime:requireModuleOrMock(mockMeScript).actual).toEqual(true)
+		end)
+	end)
+end)
