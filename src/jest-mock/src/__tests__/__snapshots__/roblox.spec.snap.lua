cfa4c628dd2c4c746319992f0af07cc9abe6a746
Daniel P H Fox (Roblox) <dfox@roblox.com>
2024-10-22 16:16:48 -0700

    APT-1914 spyOn callable tables (#413)
    ## Problem
    Callable tables are not compatible with the `spyOn` method.
    
    ## Solution
    Allow callable tables to be injected by `spyOn`, cloning their data and
    metatable while replacing the `__call` with a forwarding function to the
    mock object.
    
    ## Testing
    Unit tests and snapshots
    
    ## Checklist (remove if not applicable)
    - [x] For new modules, has a README.md file with notes about the
    translation
    - [x] All deviations are notated in code with `-- ROBLOX deviation:
    comment`

---
+++ src/jest-mock/src/__tests__/__snapshots__/roblox.spec.snap.lua
@@ -0,0 +1,6 @@
+-- Jest Roblox Snapshot v1, http://roblox.github.io/jest-roblox-internal/snapshot-testing
+local exports = {}
+exports[ [=[spyOn supported types does not allow tables with locked callable metatables 1]=] ] = [=[
+"Cannot spy the foo property because it cannot be cloned. (protected metatable)"]=]
+
+return exports
