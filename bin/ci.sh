9ca663692bed74691e7431a06d51ababd6e006b6
Daniel P H Fox (Roblox) <dfox@roblox.com>
2025-05-09 14:20:42 -0700

    Community LSP support (#453)
    ## Problem
    We still use the internal LSP to develop Jest, but would like to dogfood
    use of the community LSP.
    
    ## Solution
    Adjust the Jest project setup so that we can use the community LSP with
    Rojo and sourcemap generation.
    
    ## Testing
    Manually.
    
    ## Checklist
    ### For all changes
    - [x] Describe this change in `CHANGELOG.md`
    - [ ] [Open a PR to add/update relevant examples in
    jest-demo](https://github.com/Roblox/jest-demo)

--- bin/ci.sh
+++ bin/ci.sh
@@ -5,10 +5,10 @@ set -x
 echo "Run static analysis"
 selene src
 stylua -c src
-robloxdev-cli analyze jest.project.json --fastFlags.allOnLuau
+robloxdev-cli analyze default.project.json --fastFlags.allOnLuau
 
 echo "Run tests"
-robloxdev-cli run --load.model jest.project.json \
+robloxdev-cli run --load.model default.project.json \
   --run bin/spec.lua --testService.errorExitCode=1 \
   --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true \
   EnableSignalBehavior=true DebugForceDeferredSignalBehavior=true MaxDeferReentrancyDepth=15 \
@@ -16,13 +16,13 @@ robloxdev-cli run --load.model jest.project.json \
   --lua.globals=TESTPATHPATTERN=$1
 
 echo "Running low privilege tests"
-robloxdev-cli run --load.model jest.project.json \
+robloxdev-cli run --load.model default.project.json \
   --run bin/spec.lua --testService.errorExitCode=1 \
   --fastFlags.overrides EnableLoadModule=false --load.asRobloxScript \
   --lua.globals=TESTPATHPATTERN=$1
 
 # Uncomment this to update snapshots
-# robloxdev-cli run --load.model jest.project.json \
+# robloxdev-cli run --load.model default.project.json \
 #   --run bin/spec.lua --testService.errorExitCode=1 \
 #   --fastFlags.allOnLuau --fastFlags.overrides EnableLoadModule=true DebugDisableOptimizedBytecode=true \
 #   --lua.globals=UPDATESNAPSHOT=true --lua.globals=CI=true --load.asRobloxScript --fs.readwrite="$(pwd)"
